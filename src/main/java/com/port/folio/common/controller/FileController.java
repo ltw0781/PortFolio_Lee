b1056801e0c4017e504f41113d4f830bd36c2abb
ltw0781 <ltw0781@naver.com>
2025-11-26 20:42:11 +0900

    파일 목록 기능 추가: 파일 목록을 조회하고 표시하는 기능 구현

--- src/main/java/com/port/folio/common/controller/FileController.java
+++ src/main/java/com/port/folio/common/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.port.folio.common.controller;
 
 import java.io.File;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -109,5 +111,20 @@ public class FileController {
         return "FAIL";
 
     }
+
+    /**
+     * 파일 목록
+     * @param param parentTable, parentNo
+     * @return
+     */
+    @GetMapping("/file")
+    public String fileList(Model model, Files file ) throws Exception{
+
+        List<Files> fileList = fileService.listByParent(file);
+        model.addAttribute("fileList", fileList);
+
+        return "/file/list";
+    }
+    
 
 }
