7fa56a30ce494e659e89a51cf3b043a174ad3984
ltw0781 <ltw0781@naver.com>
2025-11-27 16:42:30 +0900

    댓글 삭제 기능 추가: 댓글 삭제 API 및 프론트엔드 로직 구현

--- src/main/java/com/port/folio/common/controller/CommentController.java
+++ src/main/java/com/port/folio/common/controller/CommentController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +54,24 @@ public class CommentController {
         model.addAttribute("commentList", commentList);
 
         return "/comment/list";
+    }
+
+    /**
+     * 댓글 삭제
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @DeleteMapping("/{id}")
+    public String deleteComment(@PathVariable("id") String id) throws Exception {
+        log.info("id : " + id);
+        int result = commentService.delete(id);
+
+        if ( result > 0 ) {
+            return "SUCCESS";
+        }
+        return "FAIL";
     }
     
     
